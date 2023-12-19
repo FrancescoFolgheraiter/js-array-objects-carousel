1f0af99bcb3201faafae0793713ffacd05bc0b28
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2023-12-19 15:28:03 +0100

    start with milestone 0

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,29 @@ Dato un array di oggetti letterali con:
  - titolo
  - descrizione
 Creare un carosello come nella foto allegata.
-Milestone 0:
-Come nel primo carosello realizzato, focalizziamoci prima sulla creazione del markup statico: costruiamo il container e inseriamo l'immagine grande in modo da poter stilare lo slider.
-Milestone 1:
-Ora rimuoviamo i contenuti statici e usiamo l'array di oggetti letterali per popolare dinamicamente il carosello.
-Al click dell'utente sulle frecce verso sinistra o destra, l'immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
-Milestone 2:
-Aggiungere il ciclo infinito del carosello. Ovvero se la slide attiva è la prima e l'utente clicca la freccia verso destra, la slide che deve attivarsi sarà l'ultima e viceversa per l'ultima slide se l'utente clicca la freccia verso sinistra.
-Buon lavoro e buon divertimento! :faccia_leggermente_sorridente:
 
+Milestone 0:
+Come nel primo carosello realizzato, focalizziamoci prima sulla creazione del markup 
+statico: costruiamo il container e inseriamo l'immagine grande in modo da poter 
+stilare lo slider.
+
+Milestone 1:
+Ora rimuoviamo i contenuti statici e usiamo l'array di oggetti letterali per 
+popolare dinamicamente il carosello.
+Al click dell'utente sulle frecce verso sinistra o destra, l'immagine attiva 
+diventerà visibile e dovremo aggiungervi titolo e testo.
+
+Milestone 2:
+Aggiungere il ciclo infinito del carosello. Ovvero se la slide attiva è la 
+prima e l'utente clicca la freccia verso destra, la slide che deve attivarsi 
+sarà l'ultima e viceversa per l'ultima slide se l'utente clicca la freccia verso sinistra.
+Buon lavoro e buon divertimento!
+
+SVOLGIMENTO
+1)creo un array che contiene degli oggetti letterali con:
+    -url
+    -titolo
+    -descrizione
+2)creo tramite markup statico il carousel in pagina index
+3)
 */
-alert("funzia")
